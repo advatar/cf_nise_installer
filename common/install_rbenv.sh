6760b22b538322bc850af7edfd230c9d5f5ada63
Johan Sellström <johan@sellstrom.me>
2014-02-23 23:07:28 +0100

    snap

--- common/install_rbenv.sh
+++ common/install_rbenv.sh
@@ -3,13 +3,13 @@
 # Install rbenv and Ruby
 apt-get -y install curl wget
 
-if [ ! -d ~/.rbenv ]; then
+if [ ! -d $HOME/.rbenv ]; then
     git clone https://github.com/sstephenson/rbenv.git /.rbenv
     git clone https://github.com/sstephenson/ruby-build.git /.rbenv/plugins/ruby-build
     echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /.profile
     echo 'eval "$(rbenv init -)"' >> /.profile
 fi
-. ~/.profile
+. $HOME/.profile
 if ! (rbenv versions | grep -q 1.9.3-p484); then
     rbenv install 1.9.3-p484
 fi
